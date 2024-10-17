ba2b7d3d724333749f584d13e41d60509cadd72f
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2024-10-17 10:44:50 -0400

    start on repl prompt

---
+++ prompt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Declare a buffer for user input of size 2048
+static char input[2048];
+
+int main(int argc, char** argv) {
+  puts("Lispy Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+
+  while (1) {
+    // output the prompt
+    fputs("lispy> ", stdout);
+
+    // read a line of user input, max size of 2048
+    fgets(input, 2048, stdin);
+
+    // echo back to user
+    printf("No You're a %s", input);
+  }
+
+  return 0;
+}
