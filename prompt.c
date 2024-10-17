a735f35d1204dad487fc6f62765a56ad15bf366f
Vadim Brodsky <VadimBrodsky@users.noreply.github.com>
2024-10-17 10:57:48 -0400

    format

--- prompt.c
+++ prompt.c
@@ -3,7 +3,7 @@
 // Declare a buffer for user input of size 2048
 static char input[2048];
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
 
