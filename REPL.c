a613d787be43c598bef21c93f775385c3fb0b88e
Vadim Brodsky <vadim.brodsky@coinbase.com>
2024-10-28 23:30:45 -0400

    support decimals in the parser

--- REPL.c
+++ REPL.c
@@ -43,9 +43,9 @@ int main(int argc, char **argv) {
 
   // define them with the following language
   mpca_lang(MPCA_LANG_DEFAULT, "                          \
-      number   : /-?[0-9]+/ ;                             \
-      operator : '+' | '-' | '*' | '/' ;                  \
-      expr     : <number> | '(' <operator>? <expr>+ ')'   \
+      number   : /-?[0-9]+('.'[0-9]+)?/ ;                 \
+      operator : '+' | '-' | '*' | '/' | '%' ;            \
+      expr     : <number> | '(' <operator>? <expr>+ ')';  \
       lispy    : /^/ <operator>? <expr>+ /$/ ;            \
     ",
             Number, Operator, Expr, Lispy);
